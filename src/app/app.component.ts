77263e19256ddcd001b9ac3ff97e9d07e7ea77d5
eller <martin@ellermeier.net>
2017-05-24 07:00:37 +0200

    Bring the hashIt function to live.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+//import { TextEncoder } from 'text-encoding/lib/encoding.js'
+import { TextEncoder, TextDecoder } from 'text-encoding-shim';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Cypto test!';
+  exampleText = "";
+  hashValue = "";
+
+  onSubmit(formData) {
+    console.log("OnSubmit");
+    this.hashIt(this.exampleText);
+
+  }
+
+  hashIt(passcode: string):void {
+
+    console.log("hashIt " + passcode)
+    let uint8array = new TextEncoder('utf-8').encode(passcode);
+    let result: any;
+    result = crypto.subtle.digest("SHA-256", uint8array)
+      .then(hash => {
+        console.log("hex == " + this.hex(hash));
+
+        this.hashValue = this.hex(hash);
+      }, (err) => {
+         console.log(err);
+         return "";
+      });
+    return result.toString();
+  }
+
+  hex(buffer: ArrayBuffer) {
+    var hexCodes = [];
+    var view = new DataView(buffer);
+    for (var i = 0; i < view.byteLength; i += 4) {
+      // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+      var value = view.getUint32(i)
+      // toString(16) will give the hex representation of the number without padding
+      var stringValue = value.toString(16)
+      // We use concatenation and slice for padding
+      var padding = '00000000'
+      var paddedValue = (padding + stringValue).slice(-padding.length)
+      hexCodes.push(paddedValue);
+    }
+
+    // Join all the hex strings into one
+    return hexCodes.join("");
+  }
 }
